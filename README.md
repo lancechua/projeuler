1e32a9840be8e4594cf86d63f5d826877bff00a7
Lance Chua <lance.chua@mbww.com>
2018-01-06 14:31:05 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Answers to some project euler problems implemented in Python
+
+https://projecteuler.net/
---
+++ projEuler.py
@@ -0,0 +1,506 @@
+import functools
+import pandas as pd
+import re, math
+import pathos.multiprocessing as mp
+import mpmath as mpm
+import scipy.misc as smsc
+from dateutil.parser import parse
+from datetime import timedelta
+
+pool = mp.Pool() # global variable for pool
+
+def is_prime(n):
+	'''reference : https://en.wikipedia.org/wiki/Primality_test'''
+	if n <= 1:
+		return False
+	elif n <= 3:
+		return True
+	elif n % 2 == 0 or n % 3 == 0:
+		return False
+	i = 5
+	while i ** 2 <= n:
+		if n % i == 0 or n % (i + 2) == 0:
+			return False
+		i+=6
+	return True
+
+
+def num2dig(y):
+	'''returns input number as a list of its digits'''
+	return [int(mpm.fmod(y, 10**(a))/ (10 ** (a-1))) for a in range(int(mpm.ceil(mpm.log(y,10))),0,-1)]
+
+def get_divs(n):
+	divs = []
+	for a in range(1,int(n**(1/2)) + 1):
+		if n % a == 0:
+			if n / a == a:
+				divs+= [int(a)]
+			else:
+				divs+= [int(a), int(n / a)]
+	return divs
+
+@functools.lru_cache()
+def fib(n):
+  if n <= 1:
+    return 1
+  else:
+    return fib(n - 1) + fib(n - 2)
+
+def prob1(x=1000):
+	'''Multiples of 3 and 5'''
+	return sum([a for a in range(1, x) if (a % 3 == 0 or a % 5 == 0)])
+
+def prob2(x=4000000):
+	'''Even Fibonacci numbers'''
+	i = 0
+	n = fib(0)
+	run_sum = 0
+	while n <= x:
+		n = fib(i)
+		if n % 2 == 0:
+			run_sum += n
+		i += 1
+	return run_sum
+
+def prob3(x=600851475143):
+	'''Largest prime factor'''
+	n = int(x ** 1 / 2) + 1
+	b = x
+	for a in range(2, n):
+		if b <= a:
+			return b
+			break
+		while True:
+			if b % a == 0:
+				b /= a
+			else:
+				break
+	return b
+
+def prob4(x=999):
+	'''Largest palindrome product'''
+	def is_palindrome(num):
+		num_str = str(int(num))
+		return num_str == num_str[::-1]
+
+	cur_res = 0
+	for a in range(x,1,-1):
+		for b in range(a,1,-1):
+			if is_palindrome(a * b):
+				cur_res = max(a * b , cur_res)
+	return cur_res
+
+
+def prob5(x=20):
+	'''Smallest multiple'''
+	primes = [2,3,5,7,11,13,17,19]
+	df = pd.DataFrame(index=primes, columns=[(a+1) for a in range(x)])
+	df.fillna(0, inplace=True)
+
+	for a in range(x):
+		cur_div = a + 1        
+		for b in primes:
+			while True:
+				if cur_div % b == 0:
+					df.loc[b, a + 1] += 1
+					cur_div /= b
+				else:
+					break
+	df = df.max(axis=1)
+	return functools.reduce(operator.mul, [ a ** df[a] for a in df.index ], 1)
+
+def prob6(x=100):
+	'''Sum square difference'''
+	return sum([a for a in range(1,x+1)]) ** 2 - sum([a ** 2 for a in range(1,x+1)]) 
+
+def prob7(x=10001):
+	'''10001st Prime'''
+	ctr = 1
+	n = 2
+	while ctr < x:
+		n+=1
+		if is_prime(n):
+			ctr+=1
+	return n
+
+def prob8(x='''73167176531330624919225119674426574742355349194934
+96983520312774506326239578318016984801869478851843
+85861560789112949495459501737958331952853208805511
+12540698747158523863050715693290963295227443043557
+66896648950445244523161731856403098711121722383113
+62229893423380308135336276614282806444486645238749
+30358907296290491560440772390713810515859307960866
+70172427121883998797908792274921901699720888093776
+65727333001053367881220235421809751254540594752243
+52584907711670556013604839586446706324415722155397
+53697817977846174064955149290862569321978468622482
+83972241375657056057490261407972968652414535100474
+82166370484403199890008895243450658541227588666881
+16427171479924442928230863465674813919123162824586
+17866458359124566529476545682848912883142607690042
+24219022671055626321111109370544217506941658960408
+07198403850962455444362981230987879927244284909188
+84580156166097919133875499200524063689912560717606
+05886116467109405077541002256983155200055935729725
+71636269561882670428252483600823257530420752963450''', adj_dig=4):
+	'''Largest product in a series'''
+	x = re.sub('\s', '', x)
+	x_list = x.split('0')
+	x_list = [a for a in x_list if len(a)>= adj_dig]
+	cur_max = 0
+	for a in x_list:
+		for b in range(len(a) - adj_dig + 1):
+			mult_str = a[b:b+adj_dig]
+			mult_dig = [int(dig) for dig in mult_str]
+			cur_max = max(cur_max, functools.reduce(operator.mul, mult_dig, 1))
+	return cur_max
+
+def prob9():
+	'''Special Pythagorean triplet'''
+	for c in range(2, 1000):
+		for b in range(2,c):
+			a = 1000 - b - c
+			if a ** 2 + b ** 2 == c ** 2:
+				return (a,b,c)
+
+def prob10(x=2000000):
+	run_sum = 0
+	for a in range(1,x):
+		if is_prime(a):
+			run_sum+=a
+	return run_sum
+
+
+def prob11(grid='', adj_dig=4):
+	if grid == '':
+		grid = '''08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48'''
+
+	mat = pd.DataFrame([list(map(int, a.split(' ')))
+						for a in grid.split('\n')])
+	cur_max = 0
+	for a in range(len(mat.index)):
+		for b in range(len(mat.columns)):            
+			prod_h = functools.reduce(operator.mul, mat.iloc[a:a+adj_dig,b]) if a <= len(mat.index) - adj_dig else 0
+			prod_v = functools.reduce(operator.mul, mat.iloc[a,b:b+adj_dig]) if b <= len(mat.columns) - adj_dig else 0
+			prod_d = functools.reduce(operator.mul, [mat.iloc[a + i,b + i] for i in range(adj_dig)]) if a <= len(mat.index) - adj_dig and b <= len(mat.columns) - adj_dig else 0
+			prod_b = functools.reduce(operator.mul, [mat.iloc[a - i,b + i] for i in range(adj_dig)]) if a > adj_dig and b <= len(mat.columns) - adj_dig else 0
+			cur_max = max(cur_max, prod_h, prod_v, prod_d, prod_b)
+	return cur_max
+
+def prob12(x=500):
+	'''Highly divisible triangular number'''
+
+	ctr = 1
+	j = 0
+	while j < x:
+		i = ctr * (ctr + 1) / 2
+		j = len(get_divs(i))
+		ctr+=1
+	return i
+
+def prob13():
+	'''Large sum'''
+	given = '''37107287533902102798797998220837590246510135740250
+46376937677490009712648124896970078050417018260538
+74324986199524741059474233309513058123726617309629
+91942213363574161572522430563301811072406154908250
+23067588207539346171171980310421047513778063246676
+89261670696623633820136378418383684178734361726757
+28112879812849979408065481931592621691275889832738
+44274228917432520321923589422876796487670272189318
+47451445736001306439091167216856844588711603153276
+70386486105843025439939619828917593665686757934951
+62176457141856560629502157223196586755079324193331
+64906352462741904929101432445813822663347944758178
+92575867718337217661963751590579239728245598838407
+58203565325359399008402633568948830189458628227828
+80181199384826282014278194139940567587151170094390
+35398664372827112653829987240784473053190104293586
+86515506006295864861532075273371959191420517255829
+71693888707715466499115593487603532921714970056938
+54370070576826684624621495650076471787294438377604
+53282654108756828443191190634694037855217779295145
+36123272525000296071075082563815656710885258350721
+45876576172410976447339110607218265236877223636045
+17423706905851860660448207621209813287860733969412
+81142660418086830619328460811191061556940512689692
+51934325451728388641918047049293215058642563049483
+62467221648435076201727918039944693004732956340691
+15732444386908125794514089057706229429197107928209
+55037687525678773091862540744969844508330393682126
+18336384825330154686196124348767681297534375946515
+80386287592878490201521685554828717201219257766954
+78182833757993103614740356856449095527097864797581
+16726320100436897842553539920931837441497806860984
+48403098129077791799088218795327364475675590848030
+87086987551392711854517078544161852424320693150332
+59959406895756536782107074926966537676326235447210
+69793950679652694742597709739166693763042633987085
+41052684708299085211399427365734116182760315001271
+65378607361501080857009149939512557028198746004375
+35829035317434717326932123578154982629742552737307
+94953759765105305946966067683156574377167401875275
+88902802571733229619176668713819931811048770190271
+25267680276078003013678680992525463401061632866526
+36270218540497705585629946580636237993140746255962
+24074486908231174977792365466257246923322810917141
+91430288197103288597806669760892938638285025333403
+34413065578016127815921815005561868836468420090470
+23053081172816430487623791969842487255036638784583
+11487696932154902810424020138335124462181441773470
+63783299490636259666498587618221225225512486764533
+67720186971698544312419572409913959008952310058822
+95548255300263520781532296796249481641953868218774
+76085327132285723110424803456124867697064507995236
+37774242535411291684276865538926205024910326572967
+23701913275725675285653248258265463092207058596522
+29798860272258331913126375147341994889534765745501
+18495701454879288984856827726077713721403798879715
+38298203783031473527721580348144513491373226651381
+34829543829199918180278916522431027392251122869539
+40957953066405232632538044100059654939159879593635
+29746152185502371307642255121183693803580388584903
+41698116222072977186158236678424689157993532961922
+62467957194401269043877107275048102390895523597457
+23189706772547915061505504953922979530901129967519
+86188088225875314529584099251203829009407770775672
+11306739708304724483816533873502340845647058077308
+82959174767140363198008187129011875491310547126581
+97623331044818386269515456334926366572897563400500
+42846280183517070527831839425882145521227251250327
+55121603546981200581762165212827652751691296897789
+32238195734329339946437501907836945765883352399886
+75506164965184775180738168837861091527357929701337
+62177842752192623401942399639168044983993173312731
+32924185707147349566916674687634660915035914677504
+99518671430235219628894890102423325116913619626622
+73267460800591547471830798392868535206946944540724
+76841822524674417161514036427982273348055556214818
+97142617910342598647204516893989422179826088076852
+87783646182799346313767754307809363333018982642090
+10848802521674670883215120185883543223812876952786
+71329612474782464538636993009049310363619763878039
+62184073572399794223406235393808339651327408011116
+66627891981488087797941876876144230030984490851411
+60661826293682836764744779239180335110989069790714
+85786944089552990653640447425576083659976645795096
+66024396409905389607120198219976047599490197230297
+64913982680032973156037120041377903785566085089252
+16730939319872750275468906903707539413042652315011
+94809377245048795150954100921645863754710598436791
+78639167021187492431995700641917969777599028300699
+15368713711936614952811305876380278410754449733078
+40789923115535562561142322423255033685442488917353
+44889911501440648020369068063960672322193204149535
+41503128880339536053299340368006977710650566631954
+81234880673210146739058568557934581403627822703280
+82616570773948327592232845941706525094512325230608
+22918802058777319719839450180888072429661980811197
+77158542502016545090413245809786882778948721859617
+72107838435069186155435662884062257473692284509516
+20849603980134001723930671666823555245252804609722
+53503534226472524250874054075591789781264330331690'''
+	nums = [mpm.mpf(a) for a in given.split('\n')]
+	return sum(nums)
+
+def prob14(n=1000000):
+	'''Longest Collatz sequence'''
+	def collatz(x):
+		col_seq = [x]
+		while True:
+			if x == 1:
+				break
+			elif x % 2 == 0:
+				x = int(x / 2)
+			else:
+				x = int(3 * x + 1)
+			col_seq.append(x)
+		return col_seq
+	col_list = pool.map( lambda x: len(collatz(x)) , range(1,n+1))
+
+	return col_list.index(max(col_list)) + 1
+
+def prob15(x=20):
+  '''Lattice Paths'''
+  return sum( a ** 2 for a in (smsc.comb(x,a) for a in range(x+1)))
+
+def prob16(x=1000):
+	'''Power digit sum'''
+	y = mpm.ldexp(1,x)
+	return sum(num2dig(y))
+
+def prob17(x=1000):
+	'''Number letter counts'''
+
+	def int2word(n, clean=True):
+		if n == 1000:
+			if clean:
+				return 'onethousand'
+			else:
+				return 'one thousand'
+
+		if n > 1000 or n < 0:
+			raise ValueError('Input must be between 1 and 1000')
+		dig_words = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+						'eight', 'nine']
+		dig2_words = ['', '', 'twenty', 'thirty', 'forty',
+						'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+		dig10s = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+					'seventeen', 'eighteen', 'nineteen']
+		num_str = ''
+		if n >= 100:
+			n_2 = int(n / 100)
+			num_str = dig_words[n_2] + ' hundred'
+			if n % 100 != 0:
+				num_str = num_str + ' and '
+
+		if n % 100 < 20 and n  % 100 > 9:
+				num_str = num_str + dig10s[(n % 100) - 10]
+		else:
+			if n % 100 >= 20:
+				num_str = num_str + dig2_words[int((n % 100) / 10)]
+			if n % 10 != 0:
+				num_str = num_str + ' ' + dig_words[int(n % 10)]
+
+		return ''.join(num_str.split(' ')) if clean else num_str
+
+	return sum(pool.map(lambda x: len(int2word(x)), range(1, x + 1)))
+
+
+def prob18(given='', val_only=False):
+	'''Maximum path sum I'''
+	if given == '':
+		given = '''75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 233'''
+	given_parse = [[int(b) for b in a.split(' ')] for a in given.split('\n')]
+	
+	def best_route(row_up, row_down, val_only = False):
+		'''
+		input:
+		row_up - list of length n containing values of that row
+		row_down - list of length n + 1 containing running sum of all rows below it
+		val_only - bool, True = get values only, False = include direction
+
+		output: list of n tuples (accumulated value, direction "L or R"),
+
+		'''
+
+		if val_only:
+			return [max(a+b, a+c) for a, b, c in zip(row_up, row_down[:-1], row_down[1:])]
+		else:
+			return [(a+b, "L") if a + b > a + c else (a+c, "R") 
+				for a,b,c in zip(row_up, row_down[:-1], row_down[1:])]
+
+	path = []
+	vals = []
+	cur_val = given_parse[-1]
+	for row in range(len(given_parse) -1):
+		if val_only:
+			cur_val = best_route(given_parse[-row-2], cur_val, val_only)
+		else:
+			return "Not yet implemented."
+
+
+	return cur_val if val_only else 0
+
+def prob19(start='1 Jan 1901', end='31 Dec 2000'):
+  '''Counting Sundays'''
+  date0 = parse(start)
+  dateN = parse(end)  
+
+  res = 0
+  dateI = date0
+  while dateI < dateN:
+    if dateI.weekday() == 6 and dateI.day == 1:
+      res += 1
+    dateI += timedelta(days=1)
+
+  return res
+
+def prob20(x=100):
+	'''Factorial digit sum'''
+	mpm.mp.dps+=200
+	y = mpm.fac(x)
+	res = sum(num2dig(y))
+	mpm.mp.dps-=200
+	return res
+
+def prob21(x=10000):
+	'''Amicable numbers'''
+	d = lambda x: sum(get_divs(x)) - x
+	return sum([a for a in range(x+1) if d(d(a)) == a and a != d(a)])
+
+def prob22(fn = 'p022_names.txt'):
+	'''Names scores'''
+
+	def name_score(name):
+		alp_str = 'abcdefghijklmnopqrstuvwxyz'
+		return sum(alp_str.index(letter.lower()) + 1 for letter in name)
+
+	data = pd.read_csv(fn, quotechar='"', lineterminator=',')
+	data = pd.DataFrame(data.columns, columns=['name']).sort_values(by='name').reset_index()
+	data['index'] = data.index + 1
+
+	data['name_score'] = data['name'].apply(name_score)
+	data['score'] = data['name_score'] * data['index']
+
+	return sum(data['score'])
+
+
+def prob25(x=1000):
+  '''1000-digit Fibonacci number'''
+  n = 1
+  y = 1
+  while mpm.log10(y) < x - 1:
+    y = fib(n)
+    n+=1
+  return n
+
+def prob28(x=1001):
+  '''Number spiral diagonals'''
+  corners = lambda x: [1] if x == 1 else [ x**2 - (x-1) * a  for a in range(4)]
+  return sum(sum(corners(a)) for a in range(1, x+1,2)) 
+
+def prob29(lb=2,ub=100):
+  '''Distinct powers'''
+  return len(set(( a ** b for a in range(lb, ub + 1) for b in range(lb, ub + 1))))
+
+def prob67(fn = 'p067_triangle.txt'):
+	'''Maximum path sum II'''
+
+	with open(fn, "r") as myfile:
+		data=myfile.read()
+
+	return prob18(given=data, val_only=True)
